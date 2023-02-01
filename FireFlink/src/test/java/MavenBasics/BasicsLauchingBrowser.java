e16fdefb592ba32ff3500a335ec82f0bccceefd5
keerthi <keerthi@Keerthika>
2023-02-01 10:26:42 +0530

    Demo project

---
+++ FireFlink/src/test/java/MavenBasics/BasicsLauchingBrowser.java
@@ -0,0 +1,25 @@
+package MavenBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasicsLauchingBrowser {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();//insteadof setproperty
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://demowebshop.tricentis.com/register");
+String parentwindow = driver.getWindowHandle();
+driver.switchTo().newWindow(WindowType.TAB);//transfer the control to next webpage
+driver.get("https://www.selenium.dev/");
+driver.switchTo().newWindow(WindowType.TAB);
+driver.get("https://in.via.com/");
+driver.switchTo().window(parentwindow);
+driver.manage().window().minimize();//selenium 4 we can minimize
+
+
+}
+}
